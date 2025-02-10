64c5173422a67e89eea787e8daca967e2a6fb149
Jordy van Domselaar <jordy.m.van.domselaar@gmail.com>
2025-02-10 18:21:31 +0100

    Initial Commit

---
+++ textrewriter/textrewriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  textrewriterApp.swift
+//  textrewriter
+//
+//  Created by Jordy van Domselaar on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct textrewriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
